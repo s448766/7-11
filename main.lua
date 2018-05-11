5ccf04648086910cf9eb711e2c8a3d6bc678c017
Faiyaz Hossain <s448766@ocsb.local>
2018-05-11 14:16:46 -0400

    shoot
    fixed

--- main.lua
+++ main.lua
@@ -100,7 +100,7 @@ local sequence_data = {
         start = 1,
         count = 10,
         time = 1000,
-        loopCount = 1,
+        loopCount = 2,
         sheet = sheetWalkingKnight
     }
 }
@@ -122,7 +122,7 @@ knight.isFixedRotation = true
 function rightArrow:touch( event )
     if ( event.phase == "ended" ) then
         -- move the character right
-        knight:setLinearVelocity( 400, 0 )
+        knight:setLinearVelocity( 800, 0 )
     knight:setSequence( "walk" )
         knight:play()
         knight.xScale = 1
@@ -134,7 +134,7 @@ end
  function leftArrow:touch( event )
     if ( event.phase == "ended" ) then
         -- move the character left
-        knight:setLinearVelocity( -400, 0 )
+        knight:setLinearVelocity( -800, 0 )
     knight:setSequence( "walk" )
         knight:play()
         knight.xScale = -1
@@ -154,6 +154,193 @@ local function resetToIdle (event)
 end
 
 
+
+
+local leftWall = display.newRect( 0, display.contentHeight / 2, 1, display.contentHeight )
+-- myRectangle.strokeWidth = 3
+-- myRectangle:setFillColor( 0.5 )
+-- myRectangle:setStrokeColor( 1, 0, 0 )
+leftWall.alpha = 0.0
+physics.addBody( leftWall, "static", { 
+    friction = 0.5, 
+    bounce = 0.3 
+    } )
+
+
+local badCharacter = display.newImage( "./assets/sprites/idle.png" )
+badCharacter.x = 1520
+badCharacter.y = display.contentHeight - 1000
+badCharacter.id = "bad character"
+physics.addBody( badCharacter, "dynamic", { 
+    friction = 0.5, 
+    bounce = 0.3 
+    } )
+
+
+ 
+local jumpButton = display.newImage( "./assets/sprites/jumpButton.png" )
+jumpButton.x = display.contentWidth - 80
+jumpButton.y = display.contentHeight - 80
+jumpButton.id = "jump button"
+jumpButton.alpha = 0.5
+
+local shootButton = display.newImage( "./assets/sprites/jumpButton.png" )
+shootButton.x = display.contentWidth - 250
+shootButton.y = display.contentHeight - 80
+shootButton.id = "shootButton"
+shootButton.alpha = 0.5
+ 
+local function characterCollision( self, event )
+ 
+    if ( event.phase == "began" ) then
+        print( self.id .. ": collision began with " .. event.other.id )
+ 
+    elseif ( event.phase == "ended" ) then
+        print( self.id .. ": collision ended with " .. event.other.id )
+    end
+end
+
+-- if character falls off the end of the world, respawn back to where it came from
+local function checkCharacterPosition( event )
+    -- check every frame to see if character has fallen
+    if knight.y > display.contentHeight + 500 then
+        knight.x = display.contentCenterX - 200
+        knight.y = display.contentCenterY
+    end
+end
+
+local function checkPlayerBulletsOutOfBounds()
+	-- check if any bullets have gone off the screen
+	local bulletCounter
+
+    if #playerBullets > 0 then
+        for bulletCounter = #playerBullets, 1 , -1 do
+            if playerBullets[bulletCounter].x > display.contentWidth + 1000 then
+                playerBullets[bulletCounter]:removeSelf()
+                playerBullets[bulletCounter] = nil
+                table.remove(playerBullets, bulletCounter)
+                print("remove bullet")
+            end
+        end
+    end
+end
+
+local function onCollision( event )
+ 
+    if ( event.phase == "began" ) then
+ 
+        local obj1 = event.object1
+        local obj2 = event.object2
+        local whereCollisonOccurredX = obj1.x
+        local whereCollisonOccurredY = obj1.y
+
+        if ( ( obj1.id == "bad character" and obj2.id == "bullet" ) or
+             ( obj1.id == "bullet" and obj2.id == "bad character" ) ) then
+            -- Remove both the laser and asteroid
+            --display.remove( obj1 )
+            --display.remove( obj2 )
+ 			
+ 			-- remove the bullet
+ 			local bulletCounter = nil
+ 			
+            for bulletCounter = #playerBullets, 1, -1 do
+                if ( playerBullets[bulletCounter] == obj1 or playerBullets[bulletCounter] == obj2 ) then
+                    playerBullets[bulletCounter]:removeSelf()
+                    playerBullets[bulletCounter] = nil
+                    table.remove( playerBullets, bulletCounter )
+                    break
+                end
+            end
+
+            --remove character
+            badCharacter:removeSelf()
+            badCharacter = nil
+
+            -- Increase score
+            print ("you could increase a score here.")
+
+            -- make an explosion sound effect
+            local expolsionSound = audio.loadStream( "./assets/sounds/8bit_bomb_explosion.wav" )
+            local explosionChannel = audio.play( expolsionSound )
+
+            -- make an explosion happen
+            -- Table of emitter parameters
+			local emitterParams = {
+			    startColorAlpha = 1,
+			    startParticleSizeVariance = 250,
+			    startColorGreen = 0.3031555,
+			    yCoordFlipped = -1,
+			    blendFuncSource = 770,
+			    rotatePerSecondVariance = 153.95,
+			    particleLifespan = 0.7237,
+			    tangentialAcceleration = -1440.74,
+			    finishColorBlue = 0.3699196,
+			    finishColorGreen = 0.5443883,
+			    blendFuncDestination = 1,
+			    startParticleSize = 400.95,
+			    startColorRed = 0.8373094,
+			    textureFileName = "./assets/sprites/idle.png",
+			    startColorVarianceAlpha = 1,
+			    maxParticles = 256,
+			    finishParticleSize = 540,
+			    duration = 0.25,
+			    finishColorRed = 1,
+			    maxRadiusVariance = 72.63,
+			    finishParticleSizeVariance = 250,
+			    gravityy = -671.05,
+			    speedVariance = 90.79,
+			    tangentialAccelVariance = -420.11,
+			    angleVariance = -142.62,
+			    angle = -244.11
+			}
+			local emitter = display.newEmitter( emitterParams )
+			emitter.x = whereCollisonOccurredX
+			emitter.y = whereCollisonOccurredY
+
+        end
+    end
+end
+
+
+function jumpButton:touch( event )
+    if ( event.phase == "ended" ) then
+        -- make the character jump
+        knight:setLinearVelocity( 0, -750 )
+    end
+
+    return true
+end
+
+function shootButton:touch( event )
+    if ( event.phase == "began" ) then
+        -- make a bullet appear
+        local aSingleBullet = display.newImage( "./assets/sprites/Kunai.png" )
+        aSingleBullet.x = knight.x
+        aSingleBullet.y = knight.y
+        physics.addBody( aSingleBullet, 'dynamic' )
+        -- Make the object a "bullet" type object
+        aSingleBullet.isBullet = true
+        aSingleBullet.isFixedRotation = true
+        aSingleBullet.gravityScale = 0
+        aSingleBullet.id = "bullet"
+        aSingleBullet:setLinearVelocity( 1500, 0 )
+
+        table.insert(playerBullets,aSingleBullet)
+        print("# of bullet: " .. tostring(#playerBullets))
+    end
+
+    return true
+end
+
+
+jumpButton:addEventListener( "touch", jumpButton )
+shootButton:addEventListener( "touch", shootButton )
+
+Runtime:addEventListener( "enterFrame", checkCharacterPosition )
+Runtime:addEventListener( "enterFrame", checkPlayerBulletsOutOfBounds )
+Runtime:addEventListener( "collision", onCollision )
+
 rightArrow:addEventListener( "touch", rightArrow )
 knight:addEventListener("sprite", resetToIdle)
 leftArrow:addEventListener( "touch", leftArrow )
+
